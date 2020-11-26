564f3bcde7b3ddab0a31d2ab32c7edc6b0f197b2
Yoann Marguerite <yoann.marguerite@orange.fr>
2020-11-26 22:20:55 +0100

    CORS

--- express/server.js
+++ express/server.js
@@ -15,7 +15,8 @@ app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+//  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://voicer-front.netlify.app');
 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
